5453ad993aab381b119f489af4fca780e7cf1b9c
Lee Haewon <hawon2205@ewhain.net>
2023-08-12 17:37:09 +0900

    [섹션 4] 4.1 컴포넌트 스캔 & DI

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,16 +2,20 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     private final MemberRepository memberRepository;
 
     // [opt + n] 눌러서 constructor 생성 - Repository를 외부에서 넣어주도록
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
