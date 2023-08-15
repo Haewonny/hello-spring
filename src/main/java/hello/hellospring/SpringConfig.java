cf950aac58c7fb5a9897d759a6630a46546246f5
Lee Haewon <hawon2205@ewhain.net>
2023-08-15 16:56:52 +0900

    [섹션 6] 6.2 Jdbc repository 연결

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,33 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository()); // 스프링 빈에 등록된 리포지토리를 memberService에 넣어줌
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository(); // 메모리를 스프링 빈으로 이용
+        return new JdbcMemberRepository(dataSource); // jdbc를 스프링 빈으로 이용
+    }
+}
