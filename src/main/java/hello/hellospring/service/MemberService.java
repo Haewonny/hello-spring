ffb830fe1c5796c257be4952e1a07b2936a31884
Lee Haewon <hawon2205@ewhain.net>
2023-08-18 16:56:04 +0900

    [섹션 7] 7.1 회원 조회 시간 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -22,12 +22,18 @@ public class MemberService {
 
     // 1. 회원가입
     public Long join(Member member) {
-        // 같은 이름이면 회원가입 불가
-        validateDuplicateMember(member);
+        long start = System.currentTimeMillis();
 
-        memberRepository.save(member);
-
-        return member.getId();
+        try {
+            // 같은 이름이면 회원가입 불가
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     // 1-1. 중복 체크
@@ -40,7 +46,15 @@ public class MemberService {
 
     // 2. 전체 회원 조회 기능
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
+
     }
 
     public Optional<Member> findOne(Long memberId) {
